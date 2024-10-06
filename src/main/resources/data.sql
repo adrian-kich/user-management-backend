b168549f7d963b37f9db12b26f1b4c31f15e6938
Adrian Kich <kich.adrian@gmail.com>
2024-10-05 21:45:45 -0300

    config: Adicionado o arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO cmn_usuarios(ID, NOME, LOGIN, SENHA, EMAIL) VALUES (1, 'Adrian Kich', 'ak', '1234', 'ak@gmail.com');
+
+ALTER SEQUENCE cmn_usuarios_id_seq RESTART WITH 2;
+
+INSERT INTO cmn_perfis(ID, DESCRICAO) VALUES (1, 'Administrador');
+INSERT INTO cmn_perfis(ID, DESCRICAO) VALUES (2, 'Operador');
+INSERT INTO cmn_perfis(ID, DESCRICAO) VALUES (3, 'Explorador');
+
+ALTER SEQUENCE cmn_perfis_id_seq RESTART WITH 4;
+
+INSERT INTO cmn_recursos (ID, NOME, CHAVE) VALUES (1, 'Tela Usuário', 'usuario');
+INSERT INTO cmn_recursos (ID, NOME, CHAVE) VALUES (2, 'Tela Perfil', 'perfil');
+INSERT INTO cmn_recursos (ID, NOME, CHAVE) VALUES (3, 'Tela Recurso', 'recurso');
+
+ALTER SEQUENCE cmn_recursos_id_seq RESTART WITH 4;
